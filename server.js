aad030228beed7b1c6dd11ee82cbe7696eda60f3
dwesh163 <ehouarn.duriaux@protonmail.com>
2024-09-25 10:31:40 +0200

    Add server.js file for setting up an Express server and serving static files

---
+++ server.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const app = express();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const distPath = path.join(__dirname, 'dist');
+
+console.log(distPath);
+
+app.get('/', (req, res) => {
+	res.status(503).sendFile(path.join(distPath, 'index.html'));
+});
+
+app.use(express.static(distPath));
+
+app.use((req, res) => {
+	res.status(503).sendFile(path.join(distPath, 'index.html'));
+});
+
+app.listen(8080, () => {
+	console.log('Server running on port 8080');
+});
