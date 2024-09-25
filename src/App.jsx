2adf262f1d570ee72fa09cf097a43be341ff8515
dwesh163 <ehouarn.duriaux@protonmail.com>
2024-09-25 10:31:36 +0200

    Add temporary maintenance page

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+	return (
+		<main>
+			<article>
+				<h1>Page temporarily unavailable</h1>
+				<p>We are performing scheduled maintenance.</p>
+			</article>
+		</main>
+	);
+}
+
+export default App;
